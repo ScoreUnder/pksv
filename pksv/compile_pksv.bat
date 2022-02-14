8c816acafc16d0c298259a61b2d75446fa2ef4fd
Score_Under <seejay.11@gmail.com>
2022-02-14 21:10:12 +0000

    Remove .bat compilation

--- pksv/compile_pksv.bat
+++
@@ -1,6 +0,0 @@
-@set path=c:\msys\mingw\bin\;..\..\bin;%PATH%
-@windres.exe -o res.o pksv.rc
-@gcc.exe -o pksv.exe pksv.c res.o -lcomdlg32
-@gcc.exe -o pksvnores.exe pksv.c -lcomdlg32
-@echo All done! Press any key to exit.
-@pause>nul
