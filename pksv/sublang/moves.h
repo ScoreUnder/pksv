bfa29ec0e783e1ed438e85186f5a069f09c5ac7e
Score_Under <seejay.11@gmail.com>
2022-02-15 04:04:23 +0000

    Start using gperf

---
+++ pksv/sublang/moves.h
@@ -0,0 +1,12 @@
+#ifndef PKSV_LANG_MOVES_H
+#define PKSV_LANG_MOVES_H 1
+
+struct move_data {
+	char const* name;
+	int value;
+};
+
+const struct move_data *
+lookup_gsc_move (register const char *str, register size_t len);
+
+#endif
