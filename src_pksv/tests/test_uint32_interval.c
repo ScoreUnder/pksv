90f4cb2d230dfc2ffe9163088b047246c0846b81
Score_Under <seejay.11@gmail.com>
2022-11-27 05:27:41 +0000

    Test delete function, fix bugs

--- src_pksv/tests/test_uint32_interval.c
+++ src_pksv/tests/test_uint32_interval.c
@@ -175,6 +175,228 @@ START_TEST(test_intervals_empty) {
 }
 END_TEST
 
+START_TEST(test_intervals_delete_simple) {
+  // Delete first
+  uint32_interval_remove(&root, 1, 10);
+
+  ck_assert_uint_eq(root.size, 4);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 0), 20);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 0), 30);
+
+  // Delete last
+  uint32_interval_remove(&root, 80, 90);
+
+  ck_assert_uint_eq(root.size, 3);
+
+  // Delete middle
+  uint32_interval_remove(&root, 40, 50);
+
+  ck_assert_uint_eq(root.size, 2);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 0), 20);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 0), 30);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 1), 60);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 1), 70);
+}
+END_TEST
+
+START_TEST(test_intervals_delete_split) {
+  uint32_interval_remove(&root, 4, 6);
+
+  ck_assert_uint_eq(root.size, 6);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 0), 1);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 0), 4);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 1), 6);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 1), 10);
+
+  uint32_interval_remove(&root, 85, 87);
+
+  ck_assert_uint_eq(root.size, 7);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 5), 80);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 5), 85);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 6), 87);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 6), 90);
+
+  uint32_interval_remove(&root, 45, 48);
+
+  ck_assert_uint_eq(root.size, 8);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 3), 40);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 3), 45);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 4), 48);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 4), 50);
+}
+END_TEST
+
+START_TEST(test_intervals_delete_trim_end) {
+  uint32_interval_remove(&root, 5, 10);
+
+  ck_assert_uint_eq(root.size, 5);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 0), 1);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 0), 5);
+
+  uint32_interval_remove(&root, 85, 95);
+
+  ck_assert_uint_eq(root.size, 5);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 4), 80);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 4), 85);
+
+  uint32_interval_remove(&root, 45, 60);
+
+  ck_assert_uint_eq(root.size, 5);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 2), 40);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 2), 45);
+}
+END_TEST
+
+START_TEST(test_intervals_delete_trim_start) {
+  uint32_interval_remove(&root, 1, 5);
+
+  ck_assert_uint_eq(root.size, 5);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 0), 5);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 0), 10);
+
+  uint32_interval_remove(&root, 70, 85);
+
+  ck_assert_uint_eq(root.size, 5);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 4), 85);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 4), 90);
+
+  uint32_interval_remove(&root, 35, 45);
+
+  ck_assert_uint_eq(root.size, 5);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 2), 45);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 2), 50);
+}
+END_TEST
+
+START_TEST(test_intervals_delete_empty) {
+  uint32_interval_remove(&root, 3, 3);
+
+  ck_assert_uint_eq(root.size, 5);
+
+  uint32_interval_remove(&root, 999, 999);
+
+  ck_assert_uint_eq(root.size, 5);
+}
+END_TEST
+
+START_TEST(test_intervals_delete_trim_both) {
+  uint32_interval_remove(&root, 5, 25);
+
+  ck_assert_uint_eq(root.size, 5);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 0), 1);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 0), 5);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 1), 25);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 1), 30);
+
+  uint32_interval_remove(&root, 65, 85);
+
+  ck_assert_uint_eq(root.size, 5);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 3), 60);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 3), 65);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 4), 85);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 4), 90);
+}
+END_TEST
+
+START_TEST(test_intervals_delete_overlap_1) {
+  // Delete overlapping a whole interval
+  uint32_interval_remove(&root, 35, 55);
+
+  ck_assert_uint_eq(root.size, 4);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 2), 60);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 2), 70);
+
+  // Same but overlapping another's start
+  uint32_interval_remove(&root, 35, 85);
+
+  ck_assert_uint_eq(root.size, 3);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 2), 85);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 2), 90);
+
+  // Same but overlapping another's end
+  uint32_interval_remove(&root, 5, 35);
+
+  ck_assert_uint_eq(root.size, 2);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 0), 1);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 0), 5);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 1), 85);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 1), 90);
+}
+END_TEST
+
+START_TEST(test_intervals_delete_overlap_2) {
+  // Delete overlapping multiple intervals
+  uint32_interval_remove(&root, 35, 1000);
+
+  ck_assert_uint_eq(root.size, 2);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 0), 1);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 0), 10);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 1), 20);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 1), 30);
+}
+END_TEST
+
+START_TEST(test_intervals_delete_overlap_3) {
+  // Delete overlapping multiple intervals
+  uint32_interval_remove(&root, 0, 55);
+
+  ck_assert_uint_eq(root.size, 2);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 0), 60);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 0), 70);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 1), 80);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 1), 90);
+}
+END_TEST
+
+START_TEST(test_intervals_delete_overlap_4) {
+  // Delete overlapping multiple intervals
+  uint32_interval_remove(&root, 0, 1000);
+
+  ck_assert_uint_eq(root.size, 0);
+}
+END_TEST
+
+START_TEST(test_intervals_delete_overlap_5) {
+  // Delete overlapping multiple intervals
+  uint32_interval_remove(&root, 1, 90);
+
+  ck_assert_uint_eq(root.size, 0);
+}
+
+START_TEST(test_intervals_delete_overlap_6) {
+  // Delete overlapping a single interval
+  uint32_interval_remove(&root, 0, 11);
+
+  ck_assert_uint_eq(root.size, 4);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 0), 20);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 0), 30);
+
+  uint32_interval_remove(&root, 75, 100);
+
+  ck_assert_uint_eq(root.size, 3);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 2), 60);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 2), 70);
+}
+
+START_TEST(test_intervals_delete_redundant) {
+  // Delete overlapping multiple intervals
+  uint32_interval_remove(&root, 10, 20);
+
+  ck_assert_uint_eq(root.size, 5);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 0), 1);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 0), 10);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 1), 20);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 1), 30);
+
+  uint32_interval_remove(&root, 15, 19);
+
+  ck_assert_uint_eq(root.size, 5);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 0), 1);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 0), 10);
+  ck_assert_uint_eq(bsearch_key_u32(&root, 1), 20);
+  ck_assert_uint_eq(bsearch_val_u32(&root, 1), 30);
+}
+
 Suite *uint32_interval_suite(void) {
   Suite *s = suite_create("uint32_interval");
   TCase *tc_core = tcase_create("Core");
@@ -187,6 +409,19 @@ Suite *uint32_interval_suite(void) {
   tcase_add_test(tc_core, test_intervals_merge_all_3);
   tcase_add_test(tc_core, test_intervals_redundant);
   tcase_add_test(tc_core, test_intervals_empty);
+  tcase_add_test(tc_core, test_intervals_delete_simple);
+  tcase_add_test(tc_core, test_intervals_delete_split);
+  tcase_add_test(tc_core, test_intervals_delete_trim_end);
+  tcase_add_test(tc_core, test_intervals_delete_trim_start);
+  tcase_add_test(tc_core, test_intervals_delete_empty);
+  tcase_add_test(tc_core, test_intervals_delete_trim_both);
+  tcase_add_test(tc_core, test_intervals_delete_overlap_1);
+  tcase_add_test(tc_core, test_intervals_delete_overlap_2);
+  tcase_add_test(tc_core, test_intervals_delete_overlap_3);
+  tcase_add_test(tc_core, test_intervals_delete_overlap_4);
+  tcase_add_test(tc_core, test_intervals_delete_overlap_5);
+  tcase_add_test(tc_core, test_intervals_delete_overlap_6);
+  tcase_add_test(tc_core, test_intervals_delete_redundant);
   suite_add_tcase(s, tc_core);
 
   return s;
--- src_pksv/uint32_interval.c
+++ src_pksv/uint32_interval.c
@@ -63,36 +63,36 @@ void uint32_interval_remove(struct bsearch_root *restrict root, uint32_t start,
   assert(end >= start);
 
   ptrdiff_t s_index = bsearch_find_u32(root, start);
+  size_t index;
   if (s_index >= 0) {
-    // Exact "start" match: trim start and put it back into the search tree
-    bsearch_replace(root, s_index, CAST_u32_pvoid(end),
-                    root->pairs[s_index].value);
+    index = (size_t)s_index;
   } else {
-    size_t index = (size_t)(-s_index - 1);
+    index = (size_t)(-s_index - 1);
+  }
 
-    while (index < root->size && bsearch_key_u32(root, index) < end) {
-      if (bsearch_val_u32(root, index) < end) {
-        // Overlap with entire later interval
-        bsearch_remove(root, index);
-      } else {
-        // Overlap with "start" of later interval -> set its start to our end
-        bsearch_replace(root, index, CAST_u32_pvoid(end),
-                        root->pairs[index].value);
-        break;
-      }
+  while (index < root->size && bsearch_key_u32(root, index) < end) {
+    uint32_t old_end = bsearch_val_u32(root, index);
+    if (old_end <= end) {
+      // Overlap with entire later interval
+      bsearch_remove(root, index);
+    } else {
+      // Overlap with "start" of later interval -> set its start to our end
+      bsearch_replace(root, index, CAST_u32_pvoid(end),
+                      CAST_u32_pvoid(old_end));
+      break;
     }
+  }
 
-    if (index > 0) {
-      uint32_t prev_key = bsearch_key_u32(root, index - 1);
-      uint32_t prev_value = bsearch_val_u32(root, index - 1);
-      assert(prev_key < start);
-      if (start < prev_value) {
-        // There is overlap, cut the interval at the end
-        bsearch_setval_u32(root, index - 1, start);
-        if (end < prev_value) {
-          // Overlap is not complete, restore the remainder as a new interval
-          bsearch_upsert_u32(root, end, prev_value);
-        }
+  if (index > 0) {
+    uint32_t prev_key = bsearch_key_u32(root, index - 1);
+    uint32_t prev_value = bsearch_val_u32(root, index - 1);
+    assert(prev_key < start);
+    if (start < prev_value) {
+      // There is overlap, cut the interval at the end
+      bsearch_setval_u32(root, index - 1, start);
+      if (end < prev_value) {
+        // Overlap is not complete, restore the remainder as a new interval
+        bsearch_upsert_u32(root, end, prev_value);
       }
     }
   }
